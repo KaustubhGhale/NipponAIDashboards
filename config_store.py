5fd82f2ae5ff712ab72d7765beb9862ef3005565
Kaustubh <kaustubhghale2004@gmail.com>
2025-06-22 00:55:16 +0530

    Testing

--- config_store.py
+++ config_store.py
@@ -1,4 +1,11 @@
 # config_store.py
 
 # Global variable to store user-submitted config
-db_config = {}
+# Mock config to bypass config check
+db_config = {
+    "host": "mock",
+    "port": 1521,
+    "service": "mock",
+    "user": "mock",
+    "password": "mock"
+}
